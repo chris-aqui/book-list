a763a5aee379b7f741402365d928110ab7630547
christine-aqui <aqui.christine@gmail.com>
2018-12-26 21:39:44 -0500

    ui function to remove book

--- app.js
+++ app.js
@@ -16,12 +16,12 @@ class UI {
       {
         title: "Book One",
         author: "John Doe",
-        isnb: "1234"
+        isbn: "1234"
       },
       {
         title: "Book Two",
         author: "Jane Doe",
-        isnb: "1111"
+        isbn: "1111"
       }
     ];
 
@@ -38,10 +38,22 @@ class UI {
     <td>${book.title}</td>
     <td>${book.author}</td>
     <td>${book.isbn}</td>
-    <td><a href="#" class="btn btn-dange btn-sm delete">X</a></td>
+    <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
     `;
 
     list.appendChild(row);
+  }
+
+  static deleteBook(el) {
+    if (el.classList.contains("delete")) {
+      el.parentElement.parentElement.remove();
+    }
+  }
+
+  static clearFields() {
+    document.querySelector("#title").value = "";
+    document.querySelector("#author").value = "";
+    document.querySelector("#isbn").value = "";
   }
 }
 
@@ -54,7 +66,7 @@ document.addEventListener("DOMContentLoaded", UI.displayBook);
 // Event Class - add book
 document.querySelector("#book-form").addEventListener("submit", e => {
   e.preventDefault();
-  // get form values
+  // get form valuesc
   const title = document.querySelector("#title").value;
   const author = document.querySelector("#author").value;
   const isbn = document.querySelector("#isbn").value;
@@ -62,6 +74,15 @@ document.querySelector("#book-form").addEventListener("submit", e => {
   //  instataite book
   const book = new Book(title, author, isbn);
   console.log(book);
+
+  // add book to UI
+  UI.addBookToList(book);
+
+  // clear feilds
+  UI.clearFields();
 });
 
 // Event - remove book
+document.querySelector("#book-list").addEventListener("click", e => {
+  UI.deleteBook(e.target);
+});
